b8b87cd86bb46b44961a4293a9cdb5a3695d3f2d
rohanmadusankavertec <vertecdeveloper.rohan@gmail.com>
2017-04-19 09:28:27 +0530

    Merge origin/master
    Conflicts:
    	src/java/com/vertec/controller/InvoiceController.java
    	web/app/invoice/CreateInvoice.jsp

--- contact.php
+++ contact.php
@@ -286,13 +286,13 @@ function mailsend() {
                         </div>-->
         </div>
         <div class="hidden-xs visible-md hidden-sm visible-lg  container-fluid" style="padding:0px; margin:0px 0px 0px 0px;">
-            <img src="image/bottom_head.png" style="width:100%;">
+            <img src="img/bottom_head.jpg" style="width:100%;">
         </div>
         <div class="hidden-xs hidden-md visible-sm hidden-lg  container-fluid" style="padding:0px; margin:0px 0px 0px 0px;">
-            <img src="image/bottom_head.png" style="width:100%;">
+            <img src="img/bottom_head.jpg" style="width:100%;">
         </div>
         <div class="hidden-lg hidden-md hidden-sm visible-xs container-fluid" style="padding:0px; margin:20px 0px 0px 0px;">
-            <img src="image/bottom_head.png" style="width:100%;">
+            <img src="img/bottom_head.jpg" style="width:100%;">
         </div>
 
         <?php include 'footer.php'; ?>
